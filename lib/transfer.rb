8d8fa3a36d0bd2ad078164d7aeae138c4ece308b
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-29 16:49:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,15 +1,17 @@
 class Transfer
   attr_accessor :amount , :status, :receiver, :sender
-
+  
  def initialize(sender,receiver,amount)
    @sender =sender
-
+   
     @amount= amount
   @receiver= receiver
   @status ="pending"
 
  end
-
+  def valid?
+    @sender.valid? &&   @reciver.valid? ? true : false
+  end
 
 
 
