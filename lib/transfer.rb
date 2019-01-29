4009965d45cd75703328c256f0ef4ae0231e33b4
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-29 16:11:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
- def initialize(funds)
+  attr_accessor :balance , :status
+  attr_reader :name, :sender, receiver
+ def initialize(funds,sender,receiver)
    @funds = funds
+   @sender =sender
+  @receiver= receiver
  end
 
 
